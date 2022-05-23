7d22cd62ca39d190de9a7c23a46a50ab43075fba
Sanskar Jaiswal <phoenix2810@protonmail.com>
2022-05-23 15:25:46 +0530

    Update
    cleaner file structure

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import pandas_datareader as web
-import datetime as dt
-import csv
 import os.path
-
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # gets rid of tensorflow warnings
-from tensorflow import keras
-from keras.models import load_model
-from itertools import zip_longest
-from sklearn.preprocessing import MinMaxScaler
-from yaml import load
-from tensorflow.keras.layers import Dense, LSTM, Dropout
+
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+from yaml import load
+from sklearn.preprocessing import MinMaxScaler
+from itertools import zip_longest
+from keras.models import load_model
+from tensorflow import keras
+import csv
+import datetime as dt
+import pandas_datareader as web
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 print("Enter the pair of currency you want to predict (e.g BTC-INR): ")
 crypto_currency, against_currency = input().split("-")
 
 start = dt.datetime(2016, 1, 1)
 end = dt.datetime.now()
-data = web.DataReader(f"{crypto_currency}-{against_currency}", "yahoo", start, end)
+data = web.DataReader(
+    f"{crypto_currency}-{against_currency}", "yahoo", start, end)
 
 # prepare data
 scaler = MinMaxScaler(feature_range=(0, 1))
@@ -36,14 +38,15 @@ else:
     x_train, y_train = [], []
 
     for x in range(prediction_days, len(scaled_data) - future_day):
-        x_train.append(scaled_data[x - prediction_days : x, 0])
+        x_train.append(scaled_data[x - prediction_days: x, 0])
         y_train.append(scaled_data[x + future_day, 0])
 
     x_train, y_train = np.array(x_train), np.array(y_train)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
     model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(LSTM(units=50, return_sequences=True,
+              input_shape=(x_train.shape[1], 1)))
     model.add(Dropout(0.1))
     model.add(LSTM(units=50, return_sequences=True))
     model.add(Dropout(0.1))
@@ -64,14 +67,14 @@ test_data = web.DataReader(
 actual_price = test_data["Close"].values
 total_dataset = pd.concat((data["Close"], test_data["Close"]), axis=0)
 model_inputs = total_dataset[
-    len(total_dataset) - len(test_data) - prediction_days :
+    len(total_dataset) - len(test_data) - prediction_days:
 ].values
 model_inputs = model_inputs.reshape(-1, 1)
 model_inputs = scaler.fit_transform(model_inputs)
 
 x_test = []
 for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x - prediction_days : x, 0])
+    x_test.append(model_inputs[x - prediction_days: x, 0])
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
@@ -93,15 +96,17 @@ plt.title(f"{crypto_currency}-{against_currency} price prediction")
 plt.xlabel("Time")
 plt.ylabel("Price")
 plt.legend(loc="upper left")
-plt.savefig(f"./graphs/{crypto_currency}-{against_currency}.png")
+plt.savefig(f"./images/{crypto_currency}-{against_currency}.png")
 
 # Predict next day
 real_data = [
-    model_inputs[len(model_inputs) + 1 - prediction_days : len(model_inputs) + 1, 0]
+    model_inputs[len(model_inputs) + 1 -
+                 prediction_days: len(model_inputs) + 1, 0]
 ]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
 prediction = model.predict(real_data)
 prediction = scaler.inverse_transform(prediction)
 prediction = prediction.flatten()
-print(f"predicted price: \n 1 {crypto_currency} ~ {prediction[0]} {against_currency}")
+print(
+    f"predicted price: \n 1 {crypto_currency} ~ {prediction[0]} {against_currency}")
